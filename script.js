fbdcdfc96ca3eb6f5c0ceb5b22dde1fd4003b2c9
lee-il-woo <empty001430@gmail.com>
2023-03-09 12:09:11 +0900

    레이아웃 기초 완성

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
-console.log(agoraStatesDiscussions);
+let form = document.querySelector('form');
+history.scrollRestoration = "auto"
+form.addEventListener('submit',(e)=>{
+  console.log(e)
+})
+
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
+
+  const postIt = document.createElement("div");
+  postIt.className = ("post-it")
 
   const avatarWrapper = document.createElement("div");
   avatarWrapper.className = "discussion__avatar--wrapper";
@@ -45,19 +53,39 @@ const convertToDiscussion = (obj) => {
   discussionContent.append(discussionTitle, discussionInformation)
 
 
-  li.append(avatarWrapper, discussionContent, discussionAnswered);
+  postIt.append(avatarWrapper, discussionContent, discussionAnswered);
   
-  return li;
+  return postIt;
 };
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
+let dataLengthCount = 0;
 const render = (element) => {
-  for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
+  for (let i = dataLengthCount; i < agoraStatesDiscussions.length; i += 1) {
     element.append(convertToDiscussion(agoraStatesDiscussions[i]));
+    dataLengthCount = dataLengthCount +1;
+    if(i !== 0 && (i+1) % 10 === 0) {
+      return
+    }
   }
   return;
 };
 
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
-const ul = document.querySelector("ul.discussions__container");
-render(ul);
+// const ul = document.querySelector("ul.discussions__container");
+// render(ul);
+const postContainer = document.querySelector(".post-container");
+render(postContainer);
+
+// 스크롤이 아래로 내려가면 글 더 불러오기
+document.addEventListener('scroll',(e)=>{
+  if((window.innerHeight + window.scrollY) >= document.body.offsetHeight){
+    render(postContainer);
+  }
+})
+
+//스크롤 리셋 버튼
+let btnTop = document.querySelector('.btn-top')
+btnTop.addEventListener('click',()=>{
+  scrollTo(0,0)
+})
